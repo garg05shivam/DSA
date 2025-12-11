e627c0a5f746cca7e96edd241d1846c2ed145935
SHIVAM GARG <148767875+garg05shivam@users.noreply.github.com>
2025-12-11 11:42:11 +0530

    easy

---
+++ reverse344.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int st = 0;
+        int e = s.size() - 1;
+
+        while (st < e) {
+            swap(s[st++], s[e--]);
+        }
+    }
+};
+
+int main() {
+    Solution obj;
+
+    int n;
+    cin >> n;
+
+    vector<char> s(n);
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+
+    obj.reverseString(s);
+
+    for (char c : s) {
+        cout << c << " ";
+    }
+
+    return 0;
+}
